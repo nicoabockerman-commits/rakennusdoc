7470103835cd5f45e510b3e81da3c523fed98f47
nicoabockerman-commits <nico.a.bockerman@gmail.com>
2026-05-20 20:46:39 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,18 @@
-const CACHE = "rakennusdoc-v3-auth";
-
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json"
-];
+const CACHE_NAME = "rakennusdoc-no-cache-v1";
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => caches.delete(key)))
+    )
   );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
-  );
+  event.respondWith(fetch(event.request));
 });
