3b9d284a0c74e1128103d5ae8b23715264022c1e
nicoabockerman-commits <nico.a.bockerman@gmail.com>
2026-05-20 19:58:34 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,23 @@
-const CACHE = 'rakennusdoc-v2';
-const ASSETS = ['/', '/index.html'];
+const CACHE_NAME = "rakennusdoc-v1";
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  if (e.request.method !== 'GET') return;
-  e.respondWith(
-    fetch(e.request)
-      .then(res => {
-        const clone = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, clone));
-        return res;
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
       })
-      .catch(() => caches.match(e.request))
   );
 });
